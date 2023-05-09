5a8eeb7e1f71c3cf249893c70b9bb5cf5eaccf6a
davidcardenasdis <david_cardenas@discovery.com>
2023-05-09 11:44:38 -0500

    remove Browsers enum and added package-info for pages.

---
+++ src/test/java/pages/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * pages package contains classes that hold the UI elements and interactions.
+ * <p>
+ * These classes contains UI locators and interactions.
+ * </p>
+ *
+ * @since 0.1
+ * @author David Cardenas
+ * @version 0.1
+ */
+package pages;
