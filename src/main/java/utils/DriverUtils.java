3eb25809ed40fda72d5b5d2e7fb156f97c062ea0
David Cardenas <davidcarguz@gmail.com>
2024-08-11 16:05:12 -0500

    removed unused import

--- src/main/java/utils/DriverUtils.java
+++ src/main/java/utils/DriverUtils.java
@@ -1,6 +1,5 @@
 package utils;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
